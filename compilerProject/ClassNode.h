6e6c22532665c61aaf6506fc019158c0aab6f72e
amer alhosary <amer.signinn@gmail.com>
2015-04-08 19:01:55 +0300

    add files

---
+++ compilerProject/ClassNode.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef __CLASSNODE__
+#define __CLASSNODE__
+#include"Node.h"
+#include"ST\Type.h"
+class ClassNode:public Node
+{
+private:
+	Type* type_node;
+public:
+	Type* get_type()
+	{
+		return type_node;
+	}
+	ClassNode() :type_node(NULL), Node(NULL,NULL,classNode)
+	{
+		
+	}
+	ClassNode(Type* t,Node* son,Node*next) :type_node(t), Node(son,next,classNode)
+	{
+		
+	}
+};
+#endif
